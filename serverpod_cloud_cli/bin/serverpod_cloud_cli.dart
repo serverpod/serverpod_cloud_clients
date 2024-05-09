b62cb9ac6131aa4ee187535d4e4186a2556e81b4
serverpod_cloud <serverpod_cloud@serverpod.dev>
2024-05-09 14:05:58 +0000

    feat: Create Serverpod Cloud CLI package. (#25)
    * fix: create serverpod cloud cli package.
    
    * chore: add serverpod cloud cli package to melos.
    
    * ci: Add serverpod cloud cli to deploy pipe for serverpod cloud clients.

---
+++ serverpod_cloud_cli/bin/serverpod_cloud_cli.dart
@@ -0,0 +1,5 @@
+import 'package:serverpod_cloud_cli/serverpod_cloud_cli.dart' as serverpod_cloud_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${serverpod_cloud_cli.calculate()}!');
+}
---
+++ serverpod_cloud_cli/test/serverpod_cloud_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:serverpod_cloud_cli/serverpod_cloud_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
