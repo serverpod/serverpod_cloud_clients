d228313e52b0b3dc55e3399c3c84635df3432038
Isakdl <isak.duner@gmail.com>
2025-02-04 13:09:57 +0100

    chore: Delete old code

--- serverpod_ground_control_client/lib/serverpod_ground_control_client.dart
+++
@@ -1,3 +0,0 @@
-export 'src/protocol/protocol.dart';
-export 'src/storage/file_uploader.dart';
-export 'package:serverpod_client/serverpod_client.dart';
--- serverpod_ground_control_client/lib/serverpod_ground_control_client_mock.dart
+++
@@ -1,1 +0,0 @@
-export 'src/mocks/client.dart';
