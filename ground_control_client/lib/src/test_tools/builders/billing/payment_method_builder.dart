3034724a0a22c042a1622abf4b11da1e753b566c
serverpod_cloud <serverpod_cloud@serverpod.dev>
2025-10-17 09:46:13 +0000

    feat(console): ad7716ca98fda8c8f6fe81b1c041f06a15efe00b

---
+++ ground_control_client/lib/src/test_tools/builders/billing/payment_method_builder.dart
@@ -0,0 +1,41 @@
+import 'package:ground_control_client/ground_control_client.dart';
+import 'package:ground_control_client/ground_control_client_test_tools.dart';
+
+class PaymentMethodBuilder {
+  String _id;
+  String _type;
+  PaymentMethodCard? _card;
+
+  PaymentMethodBuilder()
+      : _id = 'pm_test_1234567890',
+        _type = 'card',
+        _card = PaymentMethodCardBuilder().build();
+
+  PaymentMethodBuilder withId(String id) {
+    _id = id;
+    return this;
+  }
+
+  PaymentMethodBuilder withType(String type) {
+    _type = type;
+    return this;
+  }
+
+  PaymentMethodBuilder withCard(PaymentMethodCard? card) {
+    _card = card;
+    return this;
+  }
+
+  PaymentMethodBuilder withCardBuilder(PaymentMethodCardBuilder cardBuilder) {
+    _card = cardBuilder.build();
+    return this;
+  }
+
+  PaymentMethod build() {
+    return PaymentMethod(
+      id: _id,
+      type: _type,
+      card: _card,
+    );
+  }
+}
---
+++ ground_control_client/lib/src/test_tools/builders/billing/payment_method_card_builder.dart
@@ -0,0 +1,59 @@
+import 'package:ground_control_client/ground_control_client.dart';
+
+class PaymentMethodCardBuilder {
+  String _brand;
+  String _last4;
+  int _expMonth;
+  int _expYear;
+  String? _funding;
+  String? _country;
+
+  PaymentMethodCardBuilder()
+      : _brand = 'visa',
+        _last4 = '4242',
+        _expMonth = 12,
+        _expYear = 2025,
+        _funding = 'credit',
+        _country = 'US';
+
+  PaymentMethodCardBuilder withBrand(String brand) {
+    _brand = brand;
+    return this;
+  }
+
+  PaymentMethodCardBuilder withLast4(String last4) {
+    _last4 = last4;
+    return this;
+  }
+
+  PaymentMethodCardBuilder withExpMonth(int expMonth) {
+    _expMonth = expMonth;
+    return this;
+  }
+
+  PaymentMethodCardBuilder withExpYear(int expYear) {
+    _expYear = expYear;
+    return this;
+  }
+
+  PaymentMethodCardBuilder withFunding(String? funding) {
+    _funding = funding;
+    return this;
+  }
+
+  PaymentMethodCardBuilder withCountry(String? country) {
+    _country = country;
+    return this;
+  }
+
+  PaymentMethodCard build() {
+    return PaymentMethodCard(
+      brand: _brand,
+      last4: _last4,
+      expMonth: _expMonth,
+      expYear: _expYear,
+      funding: _funding,
+      country: _country,
+    );
+  }
+}
