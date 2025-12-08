2fc3c088edad70d81b3e66681488474fdd84af32
serverpod_cloud <serverpod_cloud@serverpod.dev>
2025-12-08 11:13:24 +0000

    feat: 47467f20c9a657aba01e125b3148c605f10ead4d

--- ground_control_client/lib/src/test_tools/builders/builders.dart
+++ ground_control_client/lib/src/test_tools/builders/builders.dart
@@ -5,6 +5,7 @@ export 'billing/payment_method_card_builder.dart';
 export 'billing/payment_setup_intent_builder.dart';
 export 'role_builder.dart';
 export 'project_builder.dart';
+export 'plan_info_builder.dart';
 export 'project_info_builder.dart';
 export 'subscription_info_builder.dart';
 export 'user_builder.dart';
