d90e4cdae0491002e754672f2aa8cb3f7e3745d1
serverpod_cloud <serverpod_cloud@serverpod.dev>
2026-03-06 15:16:14 +0000

    Client generated from commit d297a3b8269b2bde1bcca5c6a9041adcfa51b37a.

--- ground_control_client/lib/src/protocol/protocol.dart
+++ ground_control_client/lib/src/protocol/protocol.dart
@@ -717,6 +717,12 @@ class Protocol extends _i1.SerializationManager {
     if (t == List<double>) {
       return (data as List).map((e) => deserialize<double>(e)).toList() as T;
     }
+    if (t == Map<String, dynamic>) {
+      return (data as Map).map(
+            (k, v) => MapEntry(deserialize<String>(k), deserialize<dynamic>(v)),
+          )
+          as T;
+    }
     if (t == List<(String, String)>) {
       return (data as List)
               .map((e) => deserialize<(String, String)>(e))
