c77798ee3ab1f1dabb14744f821ef2ff49b726b5
serverpod_cloud <serverpod_cloud@serverpod.dev>
2026-03-17 15:28:09 +0000

    chore: 2cc6f95158b904cefeac1a2f7b89003e14e590b8

--- ground_control_client/lib/src/protocol/client.dart
+++ ground_control_client/lib/src/protocol/client.dart
@@ -91,6 +91,13 @@ class EndpointAdminMigration extends _i1.EndpointRef {
         'migrateProcuredDatabaseProducts',
         {},
       );
+
+  _i2.Future<Map<String, dynamic>> backfillDatabaseResources() =>
+      caller.callServerEndpoint<Map<String, dynamic>>(
+        'adminMigration',
+        'backfillDatabaseResources',
+        {},
+      );
 }
 
 /// Endpoint for global administrator to handle procurement for users.
