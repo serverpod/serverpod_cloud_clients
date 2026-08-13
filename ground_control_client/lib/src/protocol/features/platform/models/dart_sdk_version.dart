4f0e59f234de09d014a9733bd0d2183a06fbc214
serverpod_cloud <serverpod_cloud@serverpod.dev>
2026-08-13 16:30:28 +0000

    Client generated from commit b87070811073b7cd4f1bc6200f5fccda1bd2b14f.

---
+++ ground_control_client/lib/src/protocol/features/platform/models/dart_sdk_version.dart
@@ -0,0 +1,59 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: implementation_imports
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: type_literal_in_constant_pattern
+// ignore_for_file: use_super_parameters
+// ignore_for_file: invalid_use_of_internal_member
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod_client/serverpod_client.dart' as _i1;
+
+/// A Dart SDK version supported by Serverpod Cloud.
+abstract class DartSdkVersion
+    implements _i1.SerializableModel, _i1.ProtocolSerialization {
+  DartSdkVersion._({required this.version});
+
+  factory DartSdkVersion({required String version}) = _DartSdkVersionImpl;
+
+  factory DartSdkVersion.fromJson(Map<String, dynamic> jsonSerialization) {
+    return DartSdkVersion(version: jsonSerialization['version'] as String);
+  }
+
+  /// The Dart SDK minor version, such as "3.8".
+  String version;
+
+  /// Returns a shallow copy of this [DartSdkVersion]
+  /// with some or all fields replaced by the given arguments.
+  @_i1.useResult
+  DartSdkVersion copyWith({String? version});
+  @override
+  Map<String, dynamic> toJson() {
+    return {'__className__': 'DartSdkVersion', 'version': version};
+  }
+
+  @override
+  Map<String, dynamic> toJsonForProtocol() {
+    return {'__className__': 'DartSdkVersion', 'version': version};
+  }
+
+  @override
+  String toString() {
+    return _i1.SerializationManager.encode(this);
+  }
+}
+
+class _DartSdkVersionImpl extends DartSdkVersion {
+  _DartSdkVersionImpl({required String version}) : super._(version: version);
+
+  /// Returns a shallow copy of this [DartSdkVersion]
+  /// with some or all fields replaced by the given arguments.
+  @_i1.useResult
+  @override
+  DartSdkVersion copyWith({String? version}) {
+    return DartSdkVersion(version: version ?? this.version);
+  }
+}
